e320680a5e0a35312ba2b10ce6c5c500df317fff
Devis Lucato <devis@lucato.it>
2017-08-28 00:03:52 -0700

    Fix typos and scripts

--- scripts/clean-up.cmd
+++ scripts/clean-up.cmd
@@ -17,20 +17,10 @@ rmdir /s /q .\packages
 rmdir /s /q .\target
 rmdir /s /q .\out
 
-rmdir /s /q .\Services\bin
-rmdir /s /q .\Services\obj
-rmdir /s /q .\Services.Test\bin
-rmdir /s /q .\Services.Test\obj
-
-rmdir /s /q .\WebService\bin
-rmdir /s /q .\WebService\obj
-rmdir /s /q .\WebService.Test\bin
-rmdir /s /q .\WebService.Test\obj
-
-rmdir /s /q .\SimulationAgent\bin
-rmdir /s /q .\SimulationAgent\obj
-rmdir /s /q .\SimulationAgent.Test\bin
-rmdir /s /q .\SimulationAgent.Test\obj
+rmdir /s /q .\ProxyAgent\bin
+rmdir /s /q .\ProxyAgent\obj
+rmdir /s /q .\ProxyAgent.Test\bin
+rmdir /s /q .\ProxyAgent.Test\obj
 
 echo Done.
 
